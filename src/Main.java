c5fff92d8108dc59768eba6a56690eba4026f354
skfkfkvlrm <skmfmfvlrm@gmail.com>
2024-10-22 20:37:34 +0900

    test2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World22");
+        System.out.println("Hello World2211");
     }
 }
