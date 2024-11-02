90445fc9210eb41992bc94e5a9075f964791a1cb
skfkfkvlrm <skmfmfvlrm@gmail.com>
2024-11-02 11:46:11 +0900

    added

--- java2/src/dev/java/oop/abs/Plant.java
+++ java2/src/dev/java/oop/abs/Plant.java
@@ -3,4 +3,7 @@ package dev.java.oop.abs;
 public abstract class Plant {
     abstract void grow();
     abstract void photosynthesize();
+    void sayHello() {
+        System.out.println("Hello");
+    }
 }
